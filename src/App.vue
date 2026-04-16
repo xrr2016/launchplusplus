1d6e1a46238723ba2c7e0e7f6d53fb97577467af
冷石Boy <coldstoneboy@qq.com>
2026-04-17 01:13:43 +0800

    refactor: 移除未使用的sleep函数并修正产品名称大小写
    移除App.vue中未使用的sleep函数
    将tauri.conf.json中的productName从"launchplusplus"改为"LaunchPlusPlus"以保持命名一致性

--- src/App.vue
+++ src/App.vue
@@ -84,10 +84,6 @@ async function deleteConfig() {
   }
 }
 
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 let timeout = ref(10);
 
 function restartComputer() {
